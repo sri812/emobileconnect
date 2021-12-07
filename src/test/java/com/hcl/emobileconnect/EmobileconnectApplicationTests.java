17b5d4a2b30c951bc3ab97af4984b450ba638ebb
rkalavak <kalavakurirenu@gmail.com>
2021-12-07 12:07:33 +0530

    Project initial commit

---
+++ src/test/java/com/hcl/emobileconnect/EmobileconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.emobileconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmobileconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
